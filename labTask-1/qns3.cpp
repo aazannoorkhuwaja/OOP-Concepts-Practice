c05d230252613b348b98efe2ac762997a7855a71
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-01-29 21:44:05 +0500

    update

--- labTask-1/qns3.cpp
+++ labTask-1/qns3.cpp
@@ -2,31 +2,32 @@
 using namespace std;
 int main()
 {
-    int n,sq_odd,fact=1,sum_of_sq=0,sum_of_fact=0;
+    int n,sq_odd,fact=1,sq_cal=0;
+    float div_of_fact=0,div_of_sq=0;
     cout <<"Enter any number: "<<endl;
     cin>>n;
     if(n>0){
-         for(int j=n;j>0;j--)
-                {
-                    fact*=j;
+        for(int i=1;i<=n;i++)
+        {
+            int num=n-(i-1);
+        if(num%2==0){
+            int fact =1;
+            for(int j=1;j<=num;j++){
+            fact*=j;
                 }
+            div_of_fact+=(float)fact/i;
+            }
+            else{
+                sq_cal=(num)*(num);
+                div_of_sq+=(float)sq_cal/i;
+            }
+        }
 
-            for(int i=1;i<=n;i++)
-            {
-                    if (i%2==0)
-                    {
-                        sum_of_sq+=(i*i);
-                    }
-                    else{
-                        sq_odd=i*i;
-                        sum_of_fact+=fact/i;
-                    }
-            } 
-                cout<<"sum of first n terms of series is :"<<sum_of_sq+sum_of_fact<<endl;
-        }
-        else{
-            cout <<"input number greater than 0"<<endl;
-        }
+    cout <<"Series Of Given Number is :"<<div_of_fact+div_of_sq<<endl;
+       
+    }
+    else{
+        cout <<"input number greater than 0"<<endl;
+    }
     return 0;
-
 }
