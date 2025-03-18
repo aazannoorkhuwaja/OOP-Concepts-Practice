f7d08aaa56b1e4bc42a39b5b72ad67050eb12a64
Aazan Noor Khuwaja <aazannoorkhuwaja@gmail.com>
2025-03-18 20:05:59 +0500

    updated

--- labtask-4/qns2.cpp
+++ labtask-4/qns2.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <conio.h> 
 using namespace std;
-
 class toll_booth {
 private:
     unsigned int total_cars;
@@ -15,11 +14,14 @@ public:
     void paying_car() {
         total_cars++;
         total_money += 0.5;
+        cout<<"paying car: "<<total_cars<<"\nTotal Money :"<<total_money<<endl; 
     }
     void nopay_car() {
         total_cars++;
+        cout<<"non paying car: "<<total_cars<<endl;
     }
     void display() const {
+        cout<<"Final Result"<<endl;
         cout << "Total cars: " << total_cars << endl;
         cout << "Total money: " << total_money << endl;
     }
@@ -31,13 +33,12 @@ public:
         return total_money;
     }
 };
-
 int main() {
     toll_booth booth;
     char ch;
     cout << "Press 'p' for paying car, 'n' for nonpaying car, and 'Esc' to exit and display totals." << endl;
-    cin>>ch;
     while (true) {
+        ch=_getch();
         if (ch == 27) { 
             booth.display();
             break;
@@ -45,6 +46,9 @@ int main() {
             booth.paying_car();
         } else if (ch == 'n' || ch == 'N') {
             booth.nopay_car();
+        }
+        else{
+            cout<<"Input is not correct!"<<endl;
         }
     }
     return 0;
